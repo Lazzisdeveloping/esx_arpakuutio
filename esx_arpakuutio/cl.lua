6e0bb13903b77f77b0b17491d7c71dfd798740fe
Lazzis <79515120+Lazzisdeveloping@users.noreply.github.com>
2022-06-10 05:03:17 +0300

    Add files via upload

---
+++ esx_arpakuutio/cl.lua
@@ -0,0 +1,61 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+RegisterCommand('arpakuutio', function()
+    ESX.TriggerServerCallback('esx_arpakuutio:onkoluuria', function(luuri)
+        if luuri then
+            panos()
+            ExecuteCommand('e record')
+        end
+    end)
+end)
+
+function panos()
+    ESX.UI.Menu.Open(
+        'dialog', GetCurrentResourceName(), 'esx_arpakuutio',
+        {
+          title = "Kuinka paljolla haluasit pelata?"
+        },
+        function(data2, menu)
+			local bet = tonumber(data2.value)
+
+			if bet == nil or bet > 99999 then
+				ESX.ShowNotification('Virheellinen summa!')
+			else
+                TriggerServerEvent('esx_arpakuutio:checkki', bet)
+                menu.close()
+            end
+        end,
+        function(data, menu)
+        menu.close()
+      end
+    )
+end
+
+RegisterNetEvent('esx_arpakuutio:noppiensumma', function(bet)
+    ESX.UI.Menu.Open(
+        'dialog', GetCurrentResourceName(), 'esx_arpakuutio',
+        {
+          title = "Veikkauksesi noppien silmälukujen summasta? (2-12)"
+        },
+        function(data2, menu)
+			local number = tonumber(data2.value)
+
+			if number == nil or number < 2 or number > 12 then
+				ESX.ShowNotification('Virheellinen summa!')
+			else
+                TriggerServerEvent('esx_arpakuutio:pelikayntii', number, bet)
+                menu.close()
+            end
+        end,
+        function(data, menu)
+        menu.close()
+      end
+    )
+end)
---
+++ esx_arpakuutio/sv.lua
@@ -0,0 +1,45 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj)
+	ESX = obj
+end)
+
+ESX.RegisterServerCallback('esx_arpakuutio:onkoluuria', function(source, cb)
+    local xPlayer = ESX.GetPlayerFromId(source)
+  
+    if xPlayer.getInventoryItem('phone').count >= 1 then
+        cb(true)
+    else
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Sinulla ei ole puhelinta!'})
+        cb(false)
+    end
+  
+end)
+
+RegisterServerEvent('esx_arpakuutio:checkki',function(bet)
+	local pelaaja = ESX.GetPlayerFromId(source)
+	local peliraha = pelaaja.getMoney()
+    if peliraha > 0 then
+		if peliraha >= bet then
+            TriggerClientEvent('esx_arpakuutio:noppiensumma', source, bet)
+		else
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Virheellinen summa' })
+		end
+	else
+		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Virheellinen summa' })
+	end
+end)
+
+RegisterServerEvent('esx_arpakuutio:pelikayntii', function(number, bet)
+    local pelaaja = ESX.GetPlayerFromId(source)
+    local arpakuutio1 = math.random(1,6)
+    local arpakuutio2 = math.random(1,6)
+    pelaaja.removeMoney(bet)
+    TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'Betti: ' .. bet .. ' Veikkaus: ' ..number })
+    TriggerClientEvent('3dme:triggerDisplay', -1, 'Arpakuutio: \n' ..arpakuutio1..'   +   '..arpakuutio2, _source)
+    if arpakuutio1 + arpakuutio2 == number then
+        pelaaja.addMoney(bet*10)
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'Voitit: ' ..bet*10  })
+    else
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'Hävisit: ' ..bet  })
+    end
+end)
